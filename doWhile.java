f683dc1e4e2c4825c0f757d025f4461bb2073ad2
Dimpal Walde <114867886+Dimpal777@users.noreply.github.com>
2024-03-13 15:39:35 +0530

    Add files via upload

---
+++ doWhile.java
@@ -0,0 +1,11 @@
+public class doWhile {
+    public static void main(String[] args) {
+        int num = 1;
+
+        System.out.println("Numbers from 1 to 10 using do-while loop:");
+        do {
+            System.out.println(num);
+            num++;
+        } while (num <= 10);
+    }
+}
