0fd7bd542a253ed45dabcef419e6ecd32650b5cb
Dimpal Walde <114867886+Dimpal777@users.noreply.github.com>
2024-03-13 15:03:47 +0530

    Add files via upload

---
+++ EqualNotequal.java
@@ -0,0 +1,20 @@
+public class EqualNotequal {
+    public static void main(String[] args) {
+        int num1 = 40;
+        int num2 = 20;
+
+        // Using the equal operator (==)
+        if (num1 == num2) {
+            System.out.println("num1 is equal to num2");
+        } else {
+            System.out.println("num1 is not equal to num2");
+        }
+
+        // Using the not equal operator (!=)
+        if (num1 != num2) {
+            System.out.println("num1 is not equal to num2");
+        } else {
+            System.out.println("num1 is equal to num2");
+        }
+    }
+}
