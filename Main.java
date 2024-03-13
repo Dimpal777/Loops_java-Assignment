597e0146580b00453855ad2d13caf58fbf734b93
Dimpal Walde <114867886+Dimpal777@users.noreply.github.com>
2024-03-13 14:29:42 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // Loop to print "Bright IT Career" ten times
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Bright IT Career");
+        }
+    }
+}
